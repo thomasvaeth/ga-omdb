7b39762eba3634e1b696a46c8791f62cbefe4c8c
thomasvaeth <thomas.vaeth@gmail.com>
2015-10-24 12:07:20 -0700

    Updated database with correct information.

--- index.js
+++ index.js
@@ -26,4 +26,26 @@ app.get('/contact', function(req, res) {
 	res.render('contact');
 });
 
+app.post('/favorites', function(req, res) {
+	var data = req.body.id.split(',');
+	db.favorite.create({imdbId: data[0], title: data[1], year: data[2], poster: data[3]}).then(function(movie) {
+		res.redirect('/search/' + movie.imdbId);
+	});
+});
+
+app.get('/favorites', function(req, res) {
+	res.render('movies/favorites')
+});
+
 app.listen(3000);
+
+app.post('/links', function(req, res) {
+	var data = req.body.url;
+	db.link.create({url: data}).then(function(link) {
+		var hash = hashids.encode(link.id);
+		link.hash = hash;
+		link.save().then(function() {
+			res.redirect('/links/' + link.id);
+		});
+	});
+});
