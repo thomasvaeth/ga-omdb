42519da2bf067eee5dd226abe93342fbf86580b6
thomasvaeth <thomas.vaeth@gmail.com>
2015-10-24 20:23:14 -0700

    Added Rotten Tomatoes rating.

--- controllers/movie-information.js
+++ controllers/movie-information.js
@@ -4,7 +4,7 @@ var request = require('request');
 
 router.get('/:idx', function(req, res) {
 	var idx = req.params.idx;
-	request('http://www.omdbapi.com/?i=' + idx, function(error, response, body) {
+	request('http://www.omdbapi.com/?i=' + idx + '&tomatoes=true', function(error, response, body) {
 		if (!error && response.statusCode === 200) {
 			var data = JSON.parse(body);
 			res.render('movies/show', data);
