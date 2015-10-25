c17fb79b33739050cf40f5dbf9d45397e84d8d0d
thomasvaeth <thomas.vaeth@gmail.com>
2015-10-25 15:01:36 -0700

    Disabled favorite button if already favorited.

--- controllers/movie-information.js
+++ controllers/movie-information.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var db = require('../models');
 
 router.get('/:idx', function(req, res) {
 	var idx = req.params.idx;
-	request('http://www.omdbapi.com/?i=' + idx + '&tomatoes=true', function(error, response, body) {
-		if (!error && response.statusCode === 200) {
-			var data = JSON.parse(body);
-			res.render('movies/show', data);
+	var favorited = false;
+	db.favorite.find({where: {imdbId: idx}}).then(function(alreadyFavorited) {
+		if (alreadyFavorited) {
+			favorited = true;
 		}
-	});
+	}).then(function() {
+		request('http://www.omdbapi.com/?i=' + idx + '&tomatoes=true', function(error, response, body) {
+			if (!error && response.statusCode === 200) {
+				var data = JSON.parse(body);
+				res.render('movies/show', {data: data, favorited: favorited});
+			}
+		});
+	})
 });
 
 module.exports = router;
