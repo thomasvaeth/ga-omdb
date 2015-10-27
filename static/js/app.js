60cb9fa68b892f8cdd2ad7aee7d0e09dbd04b227
thomasvaeth <thomas.vaeth@gmail.com>
2015-10-26 22:31:39 -0700

    Added HTML5 property for required text fields.

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-	
-	$('form').submit(function(e) {
-		search = $('input').val();
-		if (search === '') {
-			e.preventDefault();
-			$('.btn-input').removeClass('btn-success');
-			$('.btn-input').addClass('btn-danger');
-			$('input').addClass('input-danger');
-			$('textarea').addClass('input-danger');
-		} else {
-			$('input').removeClass('input-danger');
-			$('textarea').removeClass('input-danger');
-			$('.btn-input').removeClass('btn-danger');
-			$('.btn-input').addClass('btn-success');
-		}
-	});
-
 	// Stack Overflow - disable spaces in input field
 	$('.tag-input').on({
 		keydown: function(e) {
@@ -27,5 +10,4 @@ $(document).ready(function() {
 			this.value = this.value.replace(/\s/g, '');
 		}
 	});
-
 });
