7d28271b321066c8627755a46d3945470fe234fd
thomasvaeth <thomas.vaeth@gmail.com>
2015-10-27 09:43:08 -0700

    Removed if statment.

--- controllers/movie-search.js
+++ controllers/movie-search.js
@@ -4,20 +4,16 @@ var request = require('request');
 
 router.get('/', function(req, res) {
 	var movieQuery = req.query.q;
-	if (!movieQuery) {
-		res.redirect('/');
-	} else {
-		request('http://www.omdbapi.com/?s=' + movieQuery, function(error, response, body) {
-			if (!error && response.statusCode === 200) {
-				var data = JSON.parse(body);
-				if (data.Response === "False") {
-					res.redirect('/')
-				} else {
-					res.render('movies/index', data);
-				}
+	request('http://www.omdbapi.com/?s=' + movieQuery, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var data = JSON.parse(body);
+			if (data.Response === "False") {
+				res.redirect('/')
+			} else {
+				res.render('movies/index', data);
 			}
-		});
-	}
+		}
+	});
 });
 
 module.exports = router;
